b7c321e5f16fe6f6aa2cc91a5a3a769dafd1a949
Aladdin Belhaj <110042214+AladdinBelhaj@users.noreply.github.com>
2023-02-01 16:34:22 +0100

    Add files via upload
    crud java app

---
+++ JavaProject/src/javaproject/JavaProject.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaproject;
+
+import java.sql.Statement;
+import java.sql.*;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import java.text.DateFormat;
+import java.text.MessageFormat;
+import java.text.SimpleDateFormat;
+import java.util.Vector;
+
+public class JavaProject {
+
+    /**
+     * @param args the command line arguments
+     */
+
+    
+}
